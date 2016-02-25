be9b74e65e885fa76cbfaa74292f9fb1e92deff0
django-easy-web <easy.tera.ru@gmail.com>
2016-02-25 11:23:22 +0400

    modified:   __pycache__/hello.cpython-34.pyc 	new file:   error_logs.log 	modified:   etc/gunicorn.conf 	modified:   etc/nginx.conf 	modified:   hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
+from cgi import parse_qs, escape
+
 def wsgi_application(environ, start_response):
+	d = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
 	response_body = []
-	for key, value in environ.items():
-		response_body.append(bytes("%s: %s" % (key, value), "UTF-8"))
+	for key in d:
+		for value in d[key]:
+			response_body.append(bytes("%s=%s\n" % (key, value), "UTF-8"))
 	status = '200 OK'
 	response_header = [('Content-type','text/plain')]
 	start_response(status, response_header)
-	return [response_body]
+	return response_body
