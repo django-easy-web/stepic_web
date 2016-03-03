134730860c39af43c2051ee39a8baa76e75eefc1
django-easy-web <easy.tera.ru@gmail.com>
2016-03-03 14:18:33 +0400

    Q1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, related_name='user_author')
+    likes = models.ManyToManyField(User, related_name='user_likes')
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
+
+
